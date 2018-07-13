e55b2bbcc2f5294ffb723eb89dd49f4dc7928000
Alex Steiner <privat@alexsteiner.de>
2018-07-13 14:59:40 +0200

    Add first ViewController to storyboard and month selection view

---
+++ Zeiterfassung/Views/MonthSelectionView.swift
@@ -0,0 +1,15 @@
+//
+//  MonthSelectionView.swift
+//  Zeiterfassung
+//
+//  Created by Alex Steiner on 13.07.18.
+//  Copyright © 2018 Alex Steiner. All rights reserved.
+//
+
+import UIKit
+
+class MonthSelectionView: UIView {
+
+    
+
+}
