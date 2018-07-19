798a186ed4210d18c1f5256be18d24e431177132
Alex Steiner <privat@alexsteiner.de>
2018-07-19 14:02:35 +0200

    Adds time recording view controler

---
+++ Zeiterfassung/View Controller/TimeRecordingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  TimeRecordingViewController.swift
+//  Zeiterfassung
+//
+//  Created by Alex Steiner on 19.07.18.
+//  Copyright © 2018 Alex Steiner. All rights reserved.
+//
+
+import UIKit
+
+class TimeRecordingViewController: UIViewController {
+
+    
+
+}
