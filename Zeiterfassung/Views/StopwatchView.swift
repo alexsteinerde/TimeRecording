9db12736e2cd43d83e1c2ad7297437145d2d1842
Alex Steiner <privat@alexsteiner.de>
2018-07-19 14:06:21 +0200

    Adds Stopwatch view

---
+++ Zeiterfassung/Views/StopwatchView.swift
@@ -0,0 +1,21 @@
+//
+//  StopwatchView.swift
+//  Zeiterfassung
+//
+//  Created by Alex Steiner on 19.07.18.
+//  Copyright © 2018 Alex Steiner. All rights reserved.
+//
+
+import UIKit
+
+class StopwatchView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
