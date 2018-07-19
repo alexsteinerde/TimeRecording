f2887a53581c86a15d1adb0914dddf166213305a
Alex Steiner <privat@alexsteiner.de>
2018-07-19 14:46:57 +0200

    Add setting up state view on startup

--- Zeiterfassung/View Controller/TimeRecordingViewController.swift
+++ Zeiterfassung/View Controller/TimeRecordingViewController.swift
@@ -14,8 +14,42 @@ class TimeRecordingViewController: UIViewController {
         case start
         case running
         case pause
+        
+        var view: UIView.Type {
+            switch self {
+            case .start:
+                return StartButtonView.self
+            case .running:
+                return PauseAndStopButtonView.self
+            case .pause:
+                return ContinueButtonView.self
+            }
+        }
     }
     
-    private var state: State = .start
-
+    typealias States = (current: State, old: State?)
+    
+    private var state: States = (.start, nil)
+    private weak var currentStateView: UIView?
+    
+    private func view(forState: State) -> UIView {
+        let frame = CGRect(x: 0, y: view.frame.height/3*1, width: view.frame.width, height: view.frame.height/3*2)
+        
+        let viewType = state.current.view
+        let stateView = viewType.instanceFromNib()
+        stateView.frame = frame
+        return stateView
+    }
+    
+    func replace(oldStateWith newState: State) {
+        let newStateView = view(forState: newState)
+        self.view.addSubview(newStateView)
+        currentStateView?.removeFromSuperview()
+        currentStateView = newStateView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        replace(oldStateWith: state.current)
+    }
 }
